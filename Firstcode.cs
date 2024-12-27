cb9577733c6a105ef10ca729f0298c3d2e28dbeb
DesaleRohit <rohitdesale2005@gmail.com>
2024-12-27 22:51:15 +0530

    [ADDED]:Rohit -My First C# program

---
+++ Firstcode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hello Boss");
+            System.Console.Read();
+        }
+    }
+}
