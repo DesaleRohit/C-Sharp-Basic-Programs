6f50dc826e96381ac7ade4b3e9e8a422b78c8442
DesaleRohit <rohitdesale2005@gmail.com>
2024-12-29 14:07:14 +0530

    [ADDED]:Rohit -This program for largest number

---
+++ if-else codes/bignum.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            System.Console.Write("Enter the First Number:");
+            a = System.Convert.ToInt16(System.Console.ReadLine());
+            
+             System.Console.Write("Enter the Second Number:");
+            b = System.Convert.ToInt16(System.Console.ReadLine());
+            
+             System.Console.Write("Enter the Third Number:");
+            c = System.Convert.ToInt16(System.Console.ReadLine());
+            
+            if(a > b)
+            {
+                if(a > c)
+                {
+                    System.Console.Write(a+" Biggest Number..");
+                }
+                else
+                {
+                    System.Console.Write(c+" Biggest Number..");
+                }
+            }
+            else
+            {
+                if(b > c)
+                {
+                    System.Console.Write(b+" Biggest Number..");
+                }
+                else
+                {
+                    System.Console.Write(c+" Biggest Number...");
+                }
+            }
+
+        }
+    }
+}
